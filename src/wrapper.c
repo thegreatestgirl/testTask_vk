a00c5a2c34efe8c3d0ade132054eb328a92698ba
Anna <sanni14072001@mail.ru>
2022-04-03 13:32:50 +0700

    simpe server+client

---
+++ src/wrapper.c
@@ -0,0 +1,63 @@
+#include "wrapper.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int Socket(int domain, int type, int protocol) {
+    int socketCreate;
+    socketCreate = socket(domain, type, protocol);
+    if (socketCreate == -1) {
+        perror("Can't create socket");
+        exit(EXIT_FAILURE);
+    }
+    return socketCreate;
+}
+
+void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    int res = bind(sockfd, addr, addrlen);
+    if (res == -1) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Listen(int sockfd, int backlog) {
+    int res = listen(sockfd, backlog);
+    if (res == -1) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) {
+    int res = accept(sockfd, addr, addrlen);
+    if (res == -1) {
+        perror("accept failed");
+        exit(EXIT_FAILURE);
+    }
+    return res;
+}
+
+void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    int res = connect(sockfd, addr, addrlen);
+    if (res == -1) {
+        perror("connect failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Inet_pton(int af, const char *src, void *dst) {
+    int res = inet_pton(af, src, dst);
+    if (res == 0) {
+        printf("inet_pton failed(0)\n");
+        exit(EXIT_FAILURE);
+    }
+    if (res == -1) {
+        perror("inet_pton failed(1)");
+        exit(EXIT_FAILURE);
+    }
+}
